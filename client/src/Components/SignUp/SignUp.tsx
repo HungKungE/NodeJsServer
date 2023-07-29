0ebb8fbc9ff8451694e348adf0a06f3fa4356178
HungKungE <Jonsun59@gmail.com>
2023-07-29 18:13:09 +0900

    [client] signup page 디자인 변경

--- client/src/Components/SignUp/SignUp.tsx
+++ client/src/Components/SignUp/SignUp.tsx
@@ -1,22 +1,22 @@
 import { useState } from "react";
-import { SignUpUserInfo, sendSignUpRequest } from '../../API/user';
+import { SignUpUserInfo, sendSignUpRequest } from "../../API/user";
 import { PAGE_TYPE } from "../Index/Index";
-import { useNavigate } from "react-router-dom";
 
 interface SignUpProps {
-  setPageType : (page_type:PAGE_TYPE)=> void;
+  setPageType: (page_type: PAGE_TYPE) => void;
 }
 
-const SignUp : React.FunctionComponent<SignUpProps> = ({setPageType}) => {
-  const navigate = useNavigate();
+const SignUp: React.FunctionComponent<SignUpProps> = ({ setPageType }) => {
   const [userData, setUserData] = useState<SignUpUserInfo>({
     email: "",
     password: { value: "", check: "" },
     nickname: "",
   });
 
-  const btnStyle = "border border-deeporange font-pretendardBold w-full m-[30px] px-4 py-2 rounded-[15px] text-deeporange";
-  const inputStyle = "rounded-[10px] border p-4 w-full my-[10px]";
+  const btnStyle =
+    "border border-white border-opacity-20 font-pretendardBold w-full m-[30px] px-4 py-2 rounded-[15px] text-white";
+  const inputStyle =
+    "rounded-xl border p-4 w-full my-[10px] bg-transparent  border-white text-white  placeholder-white hover:cursor-pointer";
 
   const emailReg = new RegExp(
     "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
@@ -50,23 +50,21 @@ const SignUp : React.FunctionComponent<SignUpProps> = ({setPageType}) => {
   };
 
   return (
-    <div className='relative w-full h-full flex flex-row'>
-      <div className="absolute top-0 left-[50%] -translate-x-[50%] text-[2em] font-pretendardBold justify-center text-deeporange my-[30px]">
-        회원가입
+    <div
+      className="relative w-full h-full flex flex-row bg-transparent rounded-[20px] border-solid border-2 border-white border-opacity-20"
+      style={{ backdropFilter: "blur(8px)" }}
+    >
+      <div
+        className="absolute top-0 left-[50%] -translate-x-[50%] text-[3em] font-pretendardBold justify-center text-white my-[30px] text-start"
+        style={{ fontWeight: "bold" }}
+      >
+        SignUp
       </div>
       <div className="flex flex-col w-full justify-center items-center px-[30px]">
         <input
-            className={inputStyle}
-            value={userData.email}
-            placeholder="이메일"
-            onChange={(e) => {
-              setUserData({ ...userData, email: e.target.value });
-            }}
-        />
-        <input
           className={inputStyle}
           value={userData.nickname}
-          placeholder="닉네임"
+          placeholder="Username"
           onChange={(e) => {
             setUserData({ ...userData, nickname: e.target.value });
           }}
@@ -85,9 +83,9 @@ const SignUp : React.FunctionComponent<SignUpProps> = ({setPageType}) => {
         />
         <input
           className={inputStyle}
-          type="password"
+          type="check password"
           value={userData.password.check}
-          placeholder="비밀번호"
+          placeholder="비밀번호 확인"
           onChange={(e) => {
             setUserData({
               ...userData,
@@ -95,13 +93,35 @@ const SignUp : React.FunctionComponent<SignUpProps> = ({setPageType}) => {
             });
           }}
         />
+        <input
+          className={inputStyle}
+          value={userData.email}
+          placeholder="Email"
+          onChange={(e) => {
+            setUserData({ ...userData, email: e.target.value });
+          }}
+        />
       </div>
-      <div className='absolute bottom-0 w-full h-fit flex flex-row'>
-        <button className={btnStyle} onClick={()=>{setPageType(PAGE_TYPE.SIGNIN);}}>로그인하기</button>
-        <button className={btnStyle} onClick={()=>{SignUp()}}>가입하기</button>
+      <div className="absolute bottom-0 w-full h-fit flex flex-row">
+        <button
+          className={btnStyle}
+          onClick={() => {
+            setPageType(PAGE_TYPE.SIGNIN);
+          }}
+        >
+          로그인하기
+        </button>
+        <button
+          className={btnStyle}
+          onClick={() => {
+            SignUp();
+          }}
+        >
+          가입하기
+        </button>
       </div>
     </div>
   );
-}
+};
 
 export default SignUp;
