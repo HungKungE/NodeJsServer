dcc10749b59322ff02ff1324538a84c2e066b2ba
HungKungE <Jonsun59@gmail.com>
2023-07-30 21:04:45 +0900

    [client] icon upload modal 추가

---
+++ client/src/Components/MyIcons/Modal/UploadIconsModal.tsx
@@ -0,0 +1,243 @@
+import { useRef, useState } from "react";
+
+interface UploadIconsModalProps {
+  closeModal: () => void;
+}
+
+interface UploadIconData {
+  name: string;
+  main_pic: File | undefined;
+  icon_items: File[];
+}
+
+const UploadIconsModal: React.FunctionComponent<UploadIconsModalProps> = ({
+  closeModal,
+}) => {
+  const [uploadIconData, setUploadIconData] = useState<UploadIconData>({
+    name: "",
+    main_pic: undefined,
+    icon_items: [],
+  });
+
+  const mainPictureRef = useRef<HTMLInputElement | null>(null);
+  const IconItemsRef = useRef<HTMLInputElement | null>(null);
+
+  const handleFile = (files: FileList) => {
+    const fileType = files[0].name
+      .split(".")
+      .slice(-1)
+      .toString()
+      .toLocaleUpperCase();
+
+    if (fileType !== "JPG" && fileType !== "PNG" && fileType !== "JPEG") {
+      alert("지원하지 않는 파일 포멧입니다!");
+      return;
+    }
+
+    setUploadIconData({ ...uploadIconData, main_pic: files[0] });
+  };
+
+  const handleFiles = (files: FileList) => {
+    const fileList: Set<File> = new Set();
+
+    for (let i = 0; i < files.length; i++) {
+      const fileType = files[i].name
+        .split(".")
+        .slice(-1)
+        .toString()
+        .toLocaleUpperCase();
+      if (fileType !== "JPG" && fileType !== "PNG" && fileType !== "JPEG") {
+        alert("지원하지 않는 파일 포멧입니다!");
+        return;
+      }
+      fileList.add(files[i]);
+    }
+
+    setUploadIconData({
+      ...uploadIconData,
+      icon_items: [...uploadIconData.icon_items, ...Array.from(fileList)],
+    });
+  };
+
+  const mainPictureInputClick = () => {
+    if (mainPictureRef.current) {
+      mainPictureRef.current.click();
+    }
+  };
+
+  const iconItemsInputClick = () => {
+    if (IconItemsRef.current) {
+      IconItemsRef.current.click();
+    }
+  };
+
+  const UploadIcons = () => {
+    if (
+      !uploadIconData.name.length ||
+      !uploadIconData.main_pic ||
+      !uploadIconData.icon_items.length
+    ) {
+      return;
+    }
+    /* 업로드 하는 api 추가 */
+    /* 업로드 끝나면 closeModal */
+  };
+
+  const renderUploadedFile = () => {
+    if (!uploadIconData.main_pic) {
+      return (
+        <div
+          className="border-2 w-[200px] h-[200px] text-center flex justify-center items-center hover:cursor-pointer"
+          onClick={() => {
+            mainPictureInputClick();
+          }}
+        >
+          사진 추가하기
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="border-2 rounded-xl"
+          onClick={() => {
+            mainPictureInputClick();
+          }}
+        >
+          <img
+            className="rounded-xl"
+            src={URL.createObjectURL(uploadIconData.main_pic)}
+            alt="Preview"
+            style={{ width: "200px", height: "200px" }}
+          />
+        </div>
+      );
+    }
+  };
+
+  const renderUploadedFiles = () => {
+    if (!uploadIconData.icon_items.length) {
+      return (
+        <div
+          className="border-2 px-[10px] py-[5px] flex justify-center items-center text-center w-full h-full hover:cursor-pointer"
+          onClick={() => {
+            iconItemsInputClick();
+          }}
+        >
+          사진들 추가하기
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-full h-[348px] grid grid-cols-3 gap-[10px] overflow-auto">
+          {uploadIconData.icon_items.map((icon_item, index) => {
+            return (
+              <div className="relative w-[200px] h-[200px]">
+                <img
+                  src={URL.createObjectURL(icon_item)}
+                  alt="Preview"
+                  style={{ width: "200px", height: "200px" }}
+                />
+                <div
+                  className="absolute top-[5px] right-[5px] text-red-500 border-2 border-red-500 px-[5px] hover:cursor-pointer bg-white"
+                  onClick={() => {
+                    uploadIconData.icon_items.splice(index, 1);
+                    setUploadIconData({ ...uploadIconData });
+                  }}
+                >
+                  x
+                </div>
+              </div>
+            );
+          })}
+          <div
+            className="w-[200px] h-[200px] flex justify-center items-center"
+            onClick={() => {
+              iconItemsInputClick();
+            }}
+          >
+            이미지 추가하기
+          </div>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-[800px] h-[800px] bg-white rounded-xl flex flex-col items-center">
+      <div className="w-full h-[60px] text-center relative flex flex-row justify-center border-b-2 p-[10px]">
+        <button
+          className="absolute left-5 rounded-xl bg-blue-500 text-white px-[10px] py-[5px]"
+          style={{ fontWeight: 800 }}
+          onClick={() => {
+            UploadIcons();
+          }}
+        >
+          업로드
+        </button>
+        <div
+          className="font-extralight flex items-center"
+          style={{ fontWeight: 800 }}
+        >
+          이모티콘 업로드하기
+        </div>
+        <button
+          className="absolute right-5 rounded-xl bg-red-500 text-white px-[10px] py-[5px]"
+          onClick={() => {
+            closeModal();
+          }}
+          style={{ fontWeight: 800 }}
+        >
+          닫기
+        </button>
+      </div>
+      <div className="w-full h-full flex flex-col gap-[10px] p-[10px]">
+        <div className="flex flex-row gap-2 pt-[10px] items-center">
+          <div className="font-extralight text-center">이모티콘 이름 :</div>
+          <input
+            className="border-2 px-[10px] py-[5px]"
+            value={uploadIconData.name}
+            onChange={(e) => {
+              setUploadIconData({
+                ...uploadIconData,
+                name: e.target.value,
+              });
+            }}
+          />
+        </div>
+        <div className="flex flex-row gap-2 pt-[10px] items-center">
+          <div className="font-extralight text-center">
+            이모티콘 대표 사진 :
+          </div>
+          {renderUploadedFile()}
+          <input
+            className="invisible w-[0px] h-[0px]"
+            type="file"
+            accept=".jpg, .png"
+            ref={mainPictureRef}
+            onChange={(e) => {
+              handleFile(e.target.files!);
+            }}
+          />
+        </div>
+        <div className="flex flex-col gap-2 pt-[10px]">
+          <div className="font-extralight">업로드할 이모티콘들</div>
+          <div className="w-full h-full border-2 p-[10px]">
+            {renderUploadedFiles()}
+            <input
+              className="invisible w-[0px] h-[0px]"
+              type="file"
+              multiple
+              accept=".jpg, .png"
+              ref={IconItemsRef}
+              onChange={(e) => {
+                handleFiles(e.target.files!);
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UploadIconsModal;
--- client/src/Components/MyIcons/MyIcons.tsx
+++ client/src/Components/MyIcons/MyIcons.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
 import PlusIcon from "../../Icons/Imgs/plus-svgrepo-com (2).svg";
+import UploadIconsModal from "./Modal/UploadIconsModal";
 
 const MyIcons: React.FunctionComponent = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
   return (
     <div className="flex flex-col w-full h-full px-[30px] py-[20px] items-center">
+      {openModal && (
+        <UploadIconsModal
+          closeModal={() => {
+            setOpenModal(false);
+          }}
+        ></UploadIconsModal>
+      )}
       <div className="flex flex-row w-full h-[40px] border-solid border-2 border-white border-opacity-20">
         <div className="px-[10px] h-full left-0">
-          <div className="w-[30px] h-full hover:cursor-pointer">
+          <div
+            className="w-[30px] h-full hover:cursor-pointer"
+            onClick={() => {
+              setOpenModal(true);
+            }}
+          >
             <img alt="plus_icon" src={PlusIcon}></img>
           </div>
           <div className="w-full h-full">{/* TODO : 이모티콘 종류 */}</div>
         </div>
       </div>
-      <div className="w-full h-full rounded-b-xl border-solid border-2 border-white border-opacity-20 p-[20px]"></div>
+      <div className="w-full h-full rounded-b-xl border-solid border-2 border-white border-opacity-20 p-[20px]">
+        {/* 이모티콘 사진들 */}
+      </div>
     </div>
   );
 };
